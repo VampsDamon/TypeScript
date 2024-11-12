9a242644a60df341744380d3652106814e952455
Shahid Khan <shahidseran786@gmail.com>
2024-11-12 23:43:32 +0530

    Form && useRef

--- ReactTS/src/Components/ReactFormTS/ReactFormAssign.tsx
+++ ReactTS/src/Components/ReactFormTS/ReactFormAssign.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 
 const ReactFormAssign = () => {
   return (
     <div>
 {/* <FocusInput/> */}
-<DiffEvents/>
+{/* <DiffEvents/> */}
+<ContactForm/>
     </div>
   )
 }
@@ -40,6 +41,65 @@ const DiffEvents=()=>{
       <p onClick={pHandler} ref={ptag}>Hello</p>
     </div>
   );
+}
+
+
+const ContactForm=()=>{
+  const name=useRef<HTMLInputElement>(null);
+  const email = useRef<HTMLInputElement>(null);
+  const msg = useRef<HTMLTextAreaElement>(null);
+  
+  type contactFormDataType={
+    name:string,
+    email:string,
+    msg:string,
+  }
+
+  const [contactFromData, setContactFromData] = useState<contactFormDataType[]>([]);
+
+  const handleFormSubmit=(event:FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+     const n=name.current!.value;
+     const e=email.current!.value;
+     const m=msg.current!.value;
+
+     setContactFromData((prev)=>[...prev,{
+      name:n,
+      email:e,
+      msg:m
+     }])
+  }
+
+
+
+  return <>
+   <h1>Contact Form</h1>
+   <form onSubmit={handleFormSubmit} >
+    <input type="text" ref={name} placeholder='Your name' />
+    <br/>
+    <br/>
+    <input type="email" ref={email} placeholder='Your Email' />
+    <br/>
+    <br/>
+    <textarea placeholder='Enter your Message' ref={msg} rows={8} cols={22} />
+    <br/>
+    <br/>
+    <button type="submit">Contact Me</button>
+   </form>
+   <section>
+    <h2>Contact Request</h2>
+    {
+      contactFromData.map((contact)=>{
+        return (
+          <>
+            <h3>Requested From : {contact.name} ({contact.email}) </h3>
+            <p>{contact.msg}</p>
+          </>
+        );
+      })
+    }
+   </section>
+  </>
 }
 
 export default ReactFormAssign
